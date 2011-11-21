e6eca255d133b3c5d352ba0d6dc83461a105fd13
Christopher Earl <cwearl@cs.utah.edu>
2011-11-21 12:22:52 -0700

    Simplified io.rkt

--- io.rkt
+++ io.rkt
@@ -6,34 +6,19 @@
 
 ;input/output structures, procedures, and contracts for fulmar
 
-;;;;;;;;;;;;;;
-;;;Output;;;;;
-;;;;;;;;;;;;;;
-
-;save a backup
-; Overwrites old backup file (if exists) without backing up the backup...
-(define/contract (fulmar-backup-file location)
-  (-> path? void?)
-  (if (file-exists? location)
-      (rename-file-or-directory location
-                                (string->path (string-append (path->string location) "~r"))
-                                #true)
-      (void)))
-;(provide fulmar-back-up-file)
-
 ;Open output port
+; - saves a simple backup
 (define/contract (open-output! location)
   (-> path? output-port?)
   (begin
-    (fulmar-backup-file location)
+    ; Overwrites old backup file (if exists) without backing up the backup...
+    (if (file-exists? location)
+        (rename-file-or-directory location
+                                  (string->path (string-append (path->string location) "~r~"))
+                                  #true)
+        (void))
     (open-output-file location #:exists 'replace)))
 ;(provide open-output!)
-
-;Close output port
-(define (close-output! port)
-  (-> output-port? void?)
-  (close-output-port port))
-;(provide close-output!)
 
 ;Print written code with port
 (define/contract (print-code-with-port! lines port)
@@ -53,12 +38,8 @@
       ;(path? port/location)
       (let ([port (open-output! port/location)])
         (print-file! lines port)
-        (close-output! port))))
+        (close-output-port port))))
 (provide print-file!)
-
-;;;;;;;;;;;;;;
-;;;Input;;;;;;
-;;;;;;;;;;;;;;
 
 ;open input port
 (define/contract (open-input location)
@@ -66,16 +47,6 @@
   (if (not (file-exists? location))
       (error "Could not open file at: " location)
       (open-input-file location)))
-;(test-case
-; "Test open-input - does not cover all cases"
-; (check-exn exn:fail? (λ () (open-input "none/none/asdf.ext"))))
-;(provide open-input)
-
-;close input port
-(define/contract (close-input port)
-  (-> input-port? void?)
-  (close-input-port port))
-;(provide close-input)
 
 ;TODO: change this namespace (and/or namespace-anchor) to only include core and standard chunks
 ;defintions needed for eval in read-chunks
@@ -84,24 +55,20 @@
 (define fulmar-chunk-namespace (namespace-anchor->namespace fulmar-chunk-namespace-anchor))
 ;(provide fulmar-namespace)
 
-;read chunks
-(define/contract (read-chunks port/location)
-  (-> (or/c input-port? path?) (listof chunk/c))
-  (if (input-port? port/location)
-      (let ([current (read port/location)])
-        (if (eof-object? current)
-            '()
-            (cons (eval current fulmar-chunk-namespace)
-                  (read-chunks port/location))))
-      ;(path? port/location)
-      (let* ([port (open-input port/location)]
-             [chunks (read-chunks port)])
-        (close-input port)
-        chunks)))
-;(provide read-chunks)
-
-;read file
+;read chunk
 (define/contract (read-chunk port/location)
   (-> (or/c input-port? path?) chunk/c)
+  (define (read-chunks port/location)
+    (if (input-port? port/location)
+        (let ([current (read port/location)])
+          (if (eof-object? current)
+              '()
+              (cons (eval current fulmar-chunk-namespace)
+                    (read-chunks port/location))))
+        ;(path? port/location)
+        (let* ([port (open-input port/location)]
+               [chunks (read-chunks port)])
+          (close-input-port port)
+          chunks)))
   (apply top-list-chunk (read-chunks port/location)))
 (provide read-chunk)
