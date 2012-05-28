d147edb380fad007c7353ca03ee5a235ff958ca8
Christopher Earl <cwearl@cs.utah.edu>
2012-05-28 17:07:47 -0600

    Intermediate commit - changed core-chunk.rkt somewhat - does not match tests

--- src/core-chunk.rkt
+++ src/core-chunk.rkt
@@ -9,15 +9,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;fulmar-core definitions
-(provide chunk/c)
-(provide chunk-list/c)
-(provide nullable-chunk-list/c)
 (provide flatten*)
+(provide chunk?)
+(provide chunk-list?)
+(provide nullable-chunk-list?)
 
-;TODO: increase tests to handle new contracts for combine-lengths and combine-strings (and a good number of chunks: ones that contain "*-list/c")
+;TODO: remove combine-strings, perhaps?
+;combine strings
+(define/contract (combine-strings . values)
+  (->* () #:rest literal-list? string?)
+  (foldl (λ (str total)
+           (string-append total
+                          (cond [(string? str) str]
+                                [(symbol? str) (symbol->string str)]
+                                [(pair? str) (apply combine-strings str)])))
+         ""
+         values))
+(provide combine-strings)
+
 ;combine lengths of given values
 (define/contract (combine-lengths . values)
-  (->* () #:rest length-list/c natural-number/c)
+  (->* () #:rest literal-list? natural-number/c)
   (foldl (λ (value total)
            (+ total
               (cond [(integer? value) value]
@@ -28,22 +40,10 @@
          values))
 (provide combine-lengths)
 
-;combine strings
-(define/contract (combine-strings . values)
-  (->* () #:rest string-list/c string?)
-  (foldl (λ (str total)
-           (string-append total
-                          (cond [(string? str) str]
-                                [(symbol? str) (symbol->string str)]
-                                [(pair? str) (apply combine-strings str)])))
-         ""
-         values))
-(provide combine-strings)
-
 ;helper for speculative-chunk
 ; (located here for testing)
 (define/contract (length-equals-one lst)
-  (-> written-lines/c boolean?)
+  (-> written-lines? boolean?)
   (and (pair? lst)
        (= 1 (length lst))))
 (provide length-equals-one)
@@ -51,64 +51,27 @@
 ;chunk transformer
 ; transforms chunks into nekots
 (define/contract (chunk-transform chunk context)
-  (-> chunk/c context/c nekot/c)
-  (cond [(string? chunk) (nekot 'literal
-                                chunk
-                                context)]
-        [(symbol? chunk) (nekot 'literal
-                                (symbol->string chunk)
-                                context)]
-        [(exact-nonnegative-integer? chunk) (nekot 'spaces
-                                                   chunk
-                                                   context)]
-        [(s-chunk? chunk) (let ([name (s-chunk-name chunk)]
-                                [body (s-chunk-body chunk)])
-                            (match name
-                              ['new-line       (nekot 'new-line
-                                                      null
-                                                      context)]
-                              ['pp-directive   (nekot 'pp-directive
-                                                      null
-                                                      context)]
-                              ['empty          (nekot 'empty
-                                                      null
-                                                      context)]
-                              ['concat         (nekot 'concat
-                                                      (map (λ (chunk)
-                                                             (chunk-transform chunk
-                                                                              context))
-                                                           body)
-                                                      context)]
-                              ['immediate      (nekot 'immediate
-                                                      (chunk-transform body context)
-                                                      context)]
-                              ['speculative    (nekot 'speculative
-                                                      (list (chunk-transform (first body)
-                                                                             context)
-                                                            (second body)
-                                                            (chunk-transform (third body)
-                                                                             context))
-                                                      context)]
-                              ['position-indent (nekot 'position-indent
-                                                       body
-                                                       context)]
-                              ['modify-context  (chunk-transform (first body)
-                                                                 ((second body) context))]
-                              ['comment-env   (chunk-transform (let ([env (context-env context)])
-                                                                   (concat-chunk (if (or (comment-env? env)
-                                                                                         (comment-macro-env? env)
-                                                                                         (macro-comment-env? env))
-                                                                                     (list "//"
-                                                                                           (string (second body))
-                                                                                           (modify-context-chunk (first body)
-                                                                                                                 enter-comment-env))
-                                                                                     (list "/*"
-                                                                                           (string (second body))
-                                                                                           (modify-context-chunk (first body)
-                                                                                                                 enter-comment-env)
-                                                                                           " */"))))
-                                                                 context)]
-                              [_ (error "Unknown type of s-chunk; given: " chunk)]))]
+  (-> chunk? context? logical-block?)
+  (cond [(chunk-literal? chunk) (logical-block (logical-line (literal-chunk->logical-line-IR chunk)))]
+        ;chunk-struct
+        [(chunk-struct? chunk) (let ([name (chunk-struct-name chunk)]
+                                     [body (chunk-struct-body chunk)])
+                                 (match name
+                                   ['new-line]
+                                   ['pp-directive]
+                                   ;TODO: Remove and replace with 0?
+                                   ['empty]
+                                   ['concat]
+                                   ;TODO: Remove?
+                                   ['immediate]
+                                   ;TODO: Remove?
+                                   ['speculative]
+                                   ['position-indent]
+                                   ['indent]
+                                   ['modify-context]
+                                   ;TODO: Determine if this is needed
+                                   ['comment-env]
+                                   [_ (error "Unknown type of s-chunk; given: " chunk)]))]
         [else (error "Unknown chunk subtype; given: " chunk)]))
 (provide chunk-transform)
 
@@ -119,39 +82,40 @@
 ;literal chunk
 ; simple string
 (define/contract (literal-chunk . strings)
-  (->* () #:rest string-list/c chunk/c)
+  (->* () #:rest literal-list? chunk?)
   (combine-strings strings))
 (provide literal-chunk)
 
 ;sequence of spaces chunk
 ; adds some number of spaces
 (define/contract (spaces-chunk . lengths)
-  (->* () #:rest length-list/c chunk/c)
+  (->* () #:rest literal-list? chunk?)
   (combine-lengths lengths))
 (provide spaces-chunk)
 
 ;new line chunk
 ; adds a new line
 (define/contract new-line-chunk
-  chunk/c
-  (s-chunk 'new-line
-           null))
+  chunk?
+  (chunk-struct 'new-line
+                null))
 (provide new-line-chunk)
 
 ;preprocessor directive chunk
 ; correctly adds # to line
 (define/contract pp-directive-chunk
-  chunk/c
-  (s-chunk 'pp-directive
-           null))
+  chunk?
+  (chunk-struct 'pp-directive
+                null))
 (provide pp-directive-chunk)
 
+;TODO: Remove and replace with 0, perhaps?
 ;empty (no-op) chunk
 ; only real uses of this chunk are for testing and filing in stubs/empty parameters
 (define/contract empty-chunk
-  chunk/c
-  (s-chunk 'empty
-           null))
+  chunk?
+  (chunk-struct 'empty
+                null))
 (provide empty-chunk)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -163,47 +127,49 @@
 ; - attempts to put as many on the same line as possible
 ; - no spaces added
 (define/contract (concat-chunk . chunks)
-  (->* () #:rest chunk-list/c chunk/c)
-  (s-chunk 'concat
-           (flatten chunks)))
+  (->* () #:rest chunk-list? chunk?)
+  (chunk-struct 'concat
+                (flatten* chunks)))
 (provide concat-chunk)
 
+;TODO: Remove as unnecessary with pivots, perhaps?
 ;immediate chunk
 ; bypasses usual writing rules and writes chunk immediately after preceeding chunk
 (define/contract (immediate-chunk chunk)
-  (-> chunk/c chunk/c)
-  (s-chunk 'immediate
-           chunk))
+  (-> chunk? chunk?)
+  (chunk-struct 'immediate
+                chunk))
 (provide immediate-chunk)
 
+;TODO: Remove as unnecessary with pivots, perhaps?
 ;speculative chunk
 ; attempts different chunks
 ; run proc on first chunk
 ; if proc returns true, use results of first chunk
 ; otherwise,            use results of second chunk
 (define/contract (speculative-chunk attempt success? backup)
-  (-> chunk/c (-> written-lines/c boolean?) chunk/c chunk/c)
-  (s-chunk 'speculative
-           (list attempt
-                 success?
-                 backup)))
+  (-> chunk? (-> written-lines? boolean?) chunk? chunk?)
+  (chunk-struct 'speculative
+                (list attempt
+                      success?
+                      backup)))
 (provide speculative-chunk)
 
 ;position indent chunk
 ; sets indent to current position of line
 (define/contract (position-indent-chunk chunk)
-  (-> chunk/c chunk/c)
-  (s-chunk 'position-indent
-           chunk))
+  (-> chunk? chunk?)
+  (chunk-struct 'position-indent
+                chunk))
 (provide position-indent-chunk)
 
 ;modify context chunk
 ; changes context for given chunk
 (define/contract (modify-context-chunk chunk modify)
-  (-> chunk/c (-> context/c context/c) chunk/c)
-  (s-chunk 'modify-context
-           (list chunk
-                 modify)))
+  (-> chunk? (-> context? context?) chunk?)
+  (chunk-struct 'modify-context
+                (list chunk
+                      modify)))
 (provide modify-context-chunk)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -213,26 +179,41 @@
 ;indent chunk
 ; increases current indent
 (define/contract (indent-chunk length chunk)
-  (-> length-list/c chunk/c chunk/c)
+  (-> length-list? chunk? chunk?)
   (modify-context-chunk chunk
                         (λ (context)
                           (reindent (combine-lengths length)
                                     context))))
 (provide indent-chunk)
 
+;TODO: Determine if this is needed
 ;comment env chunk
 ; puts chunks in a comment env environment
 (define/contract (comment-env-chunk chunk [char #\ ])
-  (->* (chunk/c) (char?) chunk/c)
-  (s-chunk 'comment-env
-           (list chunk
-                 char)))
+  (->* (chunk?) (char?) chunk?)
+  (chunk-struct 'comment-env
+                (list chunk
+                      char)))
 (provide comment-env-chunk)
+;['comment-env   (chunk-transform (let ([env (context-env context)])
+;                                   (concat-chunk (if (or (comment-env? env)
+;                                                         (comment-macro-env? env)
+;                                                         (macro-comment-env? env))
+;                                                     (list "//"
+;                                                           (string (second body))
+;                                                           (modify-context-chunk (first body)
+;                                                                                 enter-comment-env))
+;                                                     (list "/*"
+;                                                           (string (second body))
+;                                                           (modify-context-chunk (first body)
+;                                                                                 enter-comment-env)
+;                                                           " */"))))
+;                                 context)]
 
 ;macro environment chunk
 ; puts chunks in a macro environment
 (define/contract (macro-env-chunk chunk)
-  (-> chunk/c chunk/c)
+  (-> chunk? chunk?)
   (modify-context-chunk chunk
                         enter-macro-env))
 (provide macro-env-chunk)
