706ac1da741ca0867ac03015cbb38908cfa87b28
Christopher Earl <cwearl@cs.utah.edu>
2014-02-03 12:56:40 -0700

    Change if-empty to a macro

--- standard-chunk.rkt
+++ standard-chunk.rkt
@@ -27,10 +27,12 @@
 ;if empty
 ; returns then, if given is null (or a list that flattens to null)
 ; returns else, otherwise
-(define (if-empty given then else)
-  (if (empty? (flatten given))
-      then
-      else))
+(define-syntax if-empty
+  (syntax-rules (flatten)
+    [(if-empty given then else)
+     (if (empty? (flatten given))
+         then
+         else)]))
 (provide if-empty)
 
 ;surround/before and after chunk
@@ -81,11 +83,11 @@
 ; except: to-add is NOT added to the final chunk
 (define (attach-list-separator to-attach . chunk-lists)
   (define chunks (flatten chunk-lists))
-  (if (empty? chunks) ; can't use if-empty here - take will complain
-      null
-      (flatten* (map (λ (chunk) (concat chunk (immediate to-attach)))
-                     (take chunks (- (length chunks) 1)))
-                (last chunks))))
+  (if-empty chunks
+            null
+            (flatten* (map (λ (chunk) (concat chunk (immediate to-attach)))
+                           (take chunks (- (length chunks) 1)))
+                      (last chunks))))
 (provide attach-list-separator)
 
 ;insert a chunk between other chunks
