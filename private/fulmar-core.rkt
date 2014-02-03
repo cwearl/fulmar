f4ffd21706fe28dabf2474b3be1dc67ef59a4e35
Michael Ballantyne <michael.ballantyne@gmail.com>
2014-02-03 15:44:14 -0700

    Fix indention

--- private/fulmar-core.rkt
+++ private/fulmar-core.rkt
@@ -57,38 +57,38 @@
    [('macro 'comment)
     (macro-comment-env (environment-initial-position new))]
    [(_ _) (error "Incompatible environments combined; given: " old new)]))
-  
-  ;context Structure
-  (struct context (indent line-length env) #:transparent)
-  
-  ;construct context
-  (define (construct-context line-length)
-    (context 0
-             line-length
-             (empty-env)))
-  
-  ;new environment context
-  (define (enter-env new-env obj)
-    (struct-copy context obj [env (combine-env (context-env obj) new-env)]))
-  
-  (define (context-initial-position context)
-    (let* ([env (context-env context)])
-      (if env (environment-initial-position env) #false)))
-  
-  ;increase indent level context
-  (define (reindent new-indent obj)
-    (struct-copy context obj [indent (+ new-indent
-                                        (context-indent obj))]))
-  
-  ;new comment block
-  (define (enter-comment-env context)
-    (enter-env (comment-env (context-indent context))
-               context))
-  
-  ;new macro definition
-  (define (enter-macro-env context)
-    (enter-env macro-env
-               context))
-  
-  ;nekot Structure (reverse token - token spelled backwards)
-  (struct nekot (name body context) #:transparent)
+
+;context Structure
+(struct context (indent line-length env) #:transparent)
+
+;construct context
+(define (construct-context line-length)
+  (context 0
+           line-length
+           (empty-env)))
+
+;new environment context
+(define (enter-env new-env obj)
+  (struct-copy context obj [env (combine-env (context-env obj) new-env)]))
+
+(define (context-initial-position context)
+  (let* ([env (context-env context)])
+    (if env (environment-initial-position env) #false)))
+
+;increase indent level context
+(define (reindent new-indent obj)
+  (struct-copy context obj [indent (+ new-indent
+                                      (context-indent obj))]))
+
+;new comment block
+(define (enter-comment-env context)
+  (enter-env (comment-env (context-indent context))
+             context))
+
+;new macro definition
+(define (enter-macro-env context)
+  (enter-env macro-env
+             context))
+
+;nekot Structure (reverse token - token spelled backwards)
+(struct nekot (name body context) #:transparent)
