9bb3ae920a3808699d7ddb27f1b2bb0a78250e15
Leif Andersen <leif@leifandersen.net>
2014-10-08 11:42:47 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -3,6 +3,10 @@
 (define version "0.9.2")
 (define collection "fulmar")
 (define deps '("base"
-               "scribble-lib"))
+               "scribble-lib"
+               "typed-racket-lib"
+               "rackunit-lib"
+               "sandbox-lib"
+               "at-exp-lib"))
 (define build-deps '("at-exp-lib"))
 (define scribblings '(("doc/fulmar-doc.scrbl" ())))
